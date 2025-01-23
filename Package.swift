969a6da3060a51848837384644f1dda4d2ef2d3c
Andrey Sak <andrei.sak@banuba.com>
2025-01-23 17:38:24 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,35 +5,30 @@ import PackageDescription
 let utilsVersionRange: Range<Version> = "1.40.0"..<"1.49.99"
 
 let package = Package(
-  name: "BanubaAudioBrowserSDK",
-  platforms: [
-    .iOS(.v15)
-  ],
-  products: [
-    .library(
-      name: "BanubaAudioBrowserSDK",
-      targets: [
-        "BanubaAudioBrowserSDK",
-        "BanubaAudioBrowserSDKWrapper"
-      ]
-    )
-  ],
-  dependencies: [
-    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", utilsVersionRange),
-  ],
-  targets: [
-    .binaryTarget(
-      name: "BanubaAudioBrowserSDK",
-      path: "BanubaAudioBrowserSDK.xcframework"
-    ),
-    .target(
-        name: "BanubaAudioBrowserSDKWrapper",
-        dependencies: [
-            .product(
-                name: "BanubaUtilities",
-                package: "BanubaUtilities"
-            ),
-        ]
-    ),
-  ]
+    name: "BanubaAudioBrowserSDK",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "BanubaAudioBrowserSDK",
+            targets: [
+                "BanubaAudioBrowserSDK",
+                "BanubaUtilities"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", utilsVersionRange),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BanubaAudioBrowserSDK",
+            path: "BanubaAudioBrowserSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "BanubaUtilities",
+            path: "BanubaUtilities.xcframework"
+        ),
+    ]
 )
