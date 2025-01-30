8db173e6c6382dc59642947b6d85b4e90542d707
CI <ci@banuba.com>
2025-01-30 13:41:50 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,43 +2,43 @@
 
 import PackageDescription
 
-let utilsVersionRange: Range<Version> = "1.40.0"..<"1.49.99"
+let version: Version = "1.40.1"
 
 let package = Package(
-    name: "BanubaAudioBrowserSDK",
-    platforms: [
-        .iOS(.v15)
-    ],
-    products: [
-        .library(
-            name: "BanubaAudioBrowserSDK",
-            targets: [
-                "BanubaAudioBrowserSDKTarget"
-            ]
-        )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", utilsVersionRange),
-    ],
-    targets: [
-        .binaryTarget(
-            name: "BanubaAudioBrowserSDK",
-            path: "BanubaAudioBrowserSDK.xcframework"
-        ),
-        .target(
-          name: "BanubaAudioBrowserSDKTarget",
-          dependencies: [
-            .target(name: "BanubaAudioBrowserSDKWrapper")
-          ],
-          path: "BanubaAudioBrowserSDKTarget"
-        ),
-        .target(
-          name: "BanubaAudioBrowserSDKWrapper",
-          dependencies: [
-            "BanubaAudioBrowserSDK",
-            .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS")
-          ],
-          path: "BanubaAudioBrowserSDKWrapper"
-        )
-    ]
+  name: "BanubaAudioBrowserSDK",
+  platforms: [
+    .iOS(.v15)
+  ],
+  products: [
+    .library(
+      name: "BanubaAudioBrowserSDK",
+      targets: [
+        "BanubaAudioBrowserSDKTarget"
+      ]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", exact: version),
+  ],
+  targets: [
+    .binaryTarget(
+      name: "BanubaAudioBrowserSDK",
+      path: "BanubaAudioBrowserSDK.xcframework"
+    ),
+    .target(
+      name: "BanubaAudioBrowserSDKTarget",
+      dependencies: [
+        .target(name: "BanubaAudioBrowserSDKWrapper")
+      ],
+      path: "BanubaAudioBrowserSDKTarget"
+    ),
+    .target(
+      name: "BanubaAudioBrowserSDKWrapper",
+      dependencies: [
+        "BanubaAudioBrowserSDK",
+        .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS")
+      ],
+      path: "BanubaAudioBrowserSDKWrapper"
+    )
+  ]
 )
