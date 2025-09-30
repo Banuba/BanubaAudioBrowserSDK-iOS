bb0df32f82112b27c98b1055a04305413643b02f
CI <ci@banuba.com>
2025-09-30 16:53:00 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,21 +2,45 @@
 
 import PackageDescription
 
+let version: Version = "1.48.0"
+
 let package = Package(
   name: "BanubaAudioBrowserSDK",
   platforms: [
-    .iOS(.v17)
+    .iOS(.v15)
   ],
   products: [
     .library(
       name: "BanubaAudioBrowserSDK",
-      targets: ["BanubaAudioBrowserSDK"]
+      targets: [
+        "BanubaAudioBrowserSDKTarget"
+      ]
     )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", exact: version),
+    .package(url: "https://github.com/Banuba/BanubaLicenseServicingSDK.git", exact: version)
   ],
   targets: [
     .binaryTarget(
       name: "BanubaAudioBrowserSDK",
       path: "BanubaAudioBrowserSDK.xcframework"
+    ),
+    .target(
+      name: "BanubaAudioBrowserSDKTarget",
+      dependencies: [
+        .target(name: "BanubaAudioBrowserSDKWrapper")
+      ],
+      path: "BanubaAudioBrowserSDKTarget"
+    ),
+    .target(
+      name: "BanubaAudioBrowserSDKWrapper",
+      dependencies: [
+        "BanubaAudioBrowserSDK",
+        .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS"),
+        .product(name: "BanubaLicenseServicingSDK", package: "BanubaLicenseServicingSDK")
+      ],
+      path: "BanubaAudioBrowserSDKWrapper"
     )
   ]
 )
