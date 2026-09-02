134c99bd08d8ae7d84d6ff00cc42c2afe582607a
CI <ci@banuba.com>
2026-09-02 18:05:49 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 
 import PackageDescription
 
-let banubaUtilsVersionRange: Range<Version> = "1.54.0"..<"1.59.99"
-let banubaLicenseServicingVersionRange: Range<Version> = "1.54.0"..<"1.59.99"
+let banubaUtilsVersionRange: Range<Version> = "1.54.2"..<"1.59.99"
+let banubaLicenseServicingVersionRange: Range<Version> = "1.54.2"..<"1.59.99"
 
 let package = Package(
   name: "BanubaAudioBrowserSDK",
@@ -25,8 +25,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "BanubaAudioBrowserSDK",
-      url: "https://nexus.banuba.net/repository/ios-frameworks/ios/frameworks/BanubaAudioBrowserSDK/1.54.0/BanubaAudioBrowserSDK-1.54.0.xcframework.zip",
-      checksum: "a4c14d3f27e751919f497f467d82fb7862aa9368b1f9db8385420275898a405e"
+      url: "https://nexus.banuba.net/repository/ios-frameworks/ios/frameworks/BanubaAudioBrowserSDK/1.54.1/BanubaAudioBrowserSDK-1.54.1.xcframework.zip",
+      checksum: "a9a3552959577220aaff39e7de0faaf0f79bbefc2b8d77b516ead67dee7e9df8"
     ),
     .target(
       name: "BanubaAudioBrowserSDKTarget",
